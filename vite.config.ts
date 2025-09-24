e8c8c2583893fe07b11baba859bbad8d4a855fbe
SaiKumarReddyKodidi <SaiKumarReddyKodidi@my.unt.edu>
2025-09-24 17:10:49 -0500

    setup GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Nainika/',       // ← ensures asset URLs start with /Nainika/
   plugins: [react()],
 })
